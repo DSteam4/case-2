063ae6d2e40d4bb19dc5f0ecd903a2104822da4b
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-09-30 09:09:33 +0200

    Update Case2Rhode_new.py

--- Case2Rhode_new.py
+++ Case2Rhode_new.py
@@ -768,7 +768,7 @@ for i in fysiekearbeid:
         v = False
      
     fig2.add_trace(go.Bar(x = df_beroepsklasse_groupby.index, y = df_beroepsklasse_groupby[i],
-                        name = i, text=df_beroepsklasse_groupby[i]), visible = v)
+                        name = i, text=df_beroepsklasse_groupby[i], visible = v)
 
     dropdown_buttons.append({'label':fysiekearbeid_labels[j],
                              'method': 'update',
