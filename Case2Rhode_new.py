8c0127ce607a364a1af7eb469b12cceb4b691109
Snoozar <vince.kemme@gmail.com>
2021-09-30 10:24:28 +0200

    Update Case2Rhode_new.py

--- Case2Rhode_new.py
+++ Case2Rhode_new.py
@@ -816,7 +816,7 @@ for i in fysiekearbeid:
 
 fig2.update_traces(texttemplate='%{text:.1f}', textposition='outside')
 fig2.update_layout({'updatemenus':[{'active':True, 'buttons': dropdown_buttons,
-                                   'x': 1, 'y': 1.05}]},
+                                   'x': 1, 'y': 1.07}]},
                   annotations = [{'text':"Fysiekse arbeidsbelasting", 'font_size':15,
                                 'x': 1, 'xref':"paper", 'y':1.3, 'yref':"paper",
                                 'showarrow':False}],
